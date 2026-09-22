f67e813e01608f7ce839f99e22cf7f7bc1897edd
Godgives90 <anto_napoli_90@hotmail.it>
2026-09-22 20:14:44 +0200

    Update res_user_properties.py

--- mioprimomodulo/models/res_user_properties.py
+++ mioprimomodulo/models/res_user_properties.py
@@ -2,6 +2,6 @@ from odoo import api, fields, models
 
 class EstateProperty(models.Model):
     _inherit = "res.users"
-    _description = "Descrizione della prima inheritance del modulo di Antonio Donadio con la speranza che questo mi faccia riavere un lavoro!"
+    _description = "Descrizione della prima inheritance del modulo di Antonio D."
 
     property_ids = fields.One2many('estate.property', 'salesperson_id', domain=["|", ("state", "=", "new"), ("state", "=", "offer received")])
